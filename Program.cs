3f2db3a3476906f3de34476516a431621944333d
haileybronson <haileybronsonphotos@gmail.com>
2024-01-31 12:55:06 -0700

    setting up

--- Program.cs
+++ Program.cs
@@ -1,2 +1,55 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+//Welcome the user to the game
+using Mission4_Take2_Team0314;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Welcome to Tic-Tac-Toe!");
+        //Create a game board array to store the players’ choices
+        char[] board = { '1', '2', '3', '4', '5', '6', '7', '8', '9' };
+        char currentPlayer = 'X';
+        Supporting game = new Supporting();
+
+        while (true)
+        {
+            Console.Clear();
+            game.DrawBoard(board);
+
+            //Ask each player in turn for their choice and update the game board array
+            Console.Write($"Player {currentPlayer}, enter your move (1-9): ");
+            int move = int.Parse(Console.ReadLine());
+
+            //invalid moves
+            if (move < 1 || move > 9 || board[move - 1] == 'X' || board[move - 1] == 'O')
+            {
+                Console.WriteLine("Invalid move. Try again.");
+                continue;
+            }
+
+            board[move - 1] = currentPlayer;
+            //Check for a winner by calling the method in the supporting class, and notify the players
+
+            if (game.CheckForWinner(board, currentPlayer))
+            {
+                Console.Clear();
+                game.DrawBoard(board);
+                //when a win has occurred and which player won the game 
+                Console.WriteLine($"Player {currentPlayer} wins!");
+                break;
+            }
+
+            if (Array.IndexOf(board, ' ') == -1)
+            {
+                Console.Clear();
+                //Print the board by calling the method in the supporting class
+                game.DrawBoard(board);
+                Console.WriteLine("It's a draw!");
+                break;
+            }
+
+            currentPlayer = currentPlayer == 'X' ? 'O' : 'X';
+        }
+    }
+}
---
+++ Supporting.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Mission4_Take2_Team0314
+{
+    internal class Supporting
+    {
+        //Receive the “board” array from the driver class
+        //Contain a method that prints the board based on the information passed to it
+        public void DrawBoard(char[] board)
+        {
+            Console.WriteLine($" {board[0]} | {board[1]} | {board[2]} ");
+            Console.WriteLine("---|---|---");
+            Console.WriteLine($" {board[3]} | {board[4]} | {board[5]} ");
+            Console.WriteLine("---|---|---");
+            Console.WriteLine($" {board[6]} | {board[7]} | {board[8]} ");
+        }
+
+        /*Contain a method that receives the game board array as input and returns if there is a
+        winner and who it was*/
+        public int CheckForWinner(char[] board, int playerTurn)
+        {
+            //Horzontal Winning Condtion
+            if (board[0] == board[1] && board[1] == board[2])
+                return (playerTurn % 2 == 0) ? 2 : 1;
+            else if (board[3] == board[4] && board[4] == board[5])
+                return (playerTurn % 2 == 0) ? 2 : 1;
+            else if (board[6] == board[7] && board[7] == board[8])
+                return (playerTurn % 2 == 0) ? 2 : 1;
+
+            //Vertical Winning Condtion
+            else if (board[0] == board[3] && board[3] == board[6])
+                return (playerTurn % 2 == 0) ? 2 : 1;
+            else if (board[1] == board[4] && board[4] == board[7])
+                return (playerTurn % 2 == 0) ? 2 : 1;
+            else if (board[2] == board[5] && board[5] == board[8])
+                return (playerTurn % 2 == 0) ? 2 : 1;
+
+            //Diagonal Winning Condtion
+            else if (board[0] == board[4] && board[4] == board[8])
+                return (playerTurn % 2 == 0) ? 2 : 1;
+            else if (board[2] == board[4] && board[4] == board[6])
+                return (playerTurn % 2 == 0) ? 2 : 1;
+
+            //Checking For a Draw
+            else if (board[0] != '1' && board[1] != '2' && board[2] != '3' && board[3] != '4' &&
+                    board[4] != '5' && board[5] != '6' && board[6] != '7' && board[7] != '8' && board[8] != '9')
+                return -1;
+
+            else
+                return 0;
+        }
+    }
+}
